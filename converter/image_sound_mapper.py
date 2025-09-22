40ff6d46aaa18af39372c7f25e0cfba5535883d9
samTime101 <srijanregmi44@gmail.com>
2025-09-22 23:34:59 +0545

    refactor: broke into two classes

--- converter/image_sound_mapper.py
+++ converter/image_sound_mapper.py
@@ -13,24 +13,11 @@ import librosa
 PIXEL_RANGE = (0, 255)
 FREQ_RANGE = (200, 1000)
 DURATION_PER_PIXEL = 0.01
-
-IMAGE_FILE_PATH = "image.png"  
-GRAYSCALE_IMAGE_PATH = "grayscale_image.png"
-RESIZED_IMAGE_PATH = "resized_image.png"
-SOUND_FILE_PATH = "frequency.wav"
-SAVE_IMAGE_PATH = "frequency_image.png"
 SAMPLE_RATE = 44100
 
-class SoundImageMapper:
-    # CONSTRUCTOR
-    def __init__(
-            self,
-            image_path=IMAGE_FILE_PATH,
-            grayscale_path=GRAYSCALE_IMAGE_PATH,
-            resized_path=RESIZED_IMAGE_PATH,
-            sound_path=SOUND_FILE_PATH,
-            save_path=SAVE_IMAGE_PATH,
-        ) -> None :
+class ImageToSound:
+
+    def __init__( self, image_path, grayscale_path, resized_path, sound_path) -> None :
         # FIRST CHECK IF IMAGE_FILE_PATH EXISTS
         try:
             if not os.path.isfile(image_path):
@@ -40,7 +27,7 @@ class SoundImageMapper:
             return
         
         # CHECK IF DIRECTORIES EXIST, IF NOT CREATE THEM
-        for path in [grayscale_path, resized_path, sound_path, save_path]:
+        for path in [grayscale_path, resized_path, sound_path]:
             dir_name = os.path.dirname(path)
             if dir_name and not os.path.exists(dir_name):
                 os.makedirs(dir_name)
@@ -50,11 +37,10 @@ class SoundImageMapper:
         self.grayscale_path = grayscale_path
         self.resized_path = resized_path
         self.sound_path = sound_path
-        self.save_path = save_path
-
 
     # GET IMAGE DIMENSIONS
     def get_image_dimensions(self) -> tuple | None:
+        print("1. GETTING IMAGE DIMENSIONS...")
         try:
             with Image.open(self.image_path) as img:
                 width, height = img.size
@@ -65,6 +51,7 @@ class SoundImageMapper:
 
     # RESIZE IMAGE
     def compress_image_with_new_dimension(self, new_dimensions: tuple) -> None:
+        print("2. RESIZING IMAGE...")
         try:
             with Image.open(self.image_path) as img:
                 img = img.resize(new_dimensions)
@@ -74,6 +61,7 @@ class SoundImageMapper:
 
     # CONVERT TO GRAYSCALE
     def image_to_grayscale(self) -> None:
+        print("3. CONVERTING RESIZED IMAGE TO GRAYSCALE...")
         try:
             img = Image.open(self.resized_path).convert("L")
             img.save(self.grayscale_path)
@@ -82,6 +70,7 @@ class SoundImageMapper:
     
     # EXTRACT PIXELS
     def pixel_value_extractor(self) -> list:
+        print("4. EXTRACTING PIXEL VALUES FROM GRAYSCALE IMAGE...")
         try:
             with Image.open(self.grayscale_path) as img:
                 pixels = list(img.getdata())
@@ -92,6 +81,7 @@ class SoundImageMapper:
 
     # MAP PIXELS TO FREQUENCIES
     def pixel_to_frequency(self, pixel_data: list) -> list:
+        print("5. MAPPING PIXELS TO FREQUENCIES...")
         f_min, f_max = FREQ_RANGE
         p_min, p_max = PIXEL_RANGE
 
@@ -101,20 +91,11 @@ class SoundImageMapper:
             for px in pixel_data
         ]
         return freqs
-
-    # MAP FREQUENCIES BACK TO PIXELS
-    def frequency_to_pixel(self, frequency_data: list) -> list:
-        f_min, f_max = FREQ_RANGE
-        p_min, p_max = PIXEL_RANGE
-        pixels = [
-            p_min + ((freq - f_min) / (f_max - f_min)) * (p_max - p_min)
-            for freq in frequency_data
-        ]
-        return pixels
-
+    
     # CONVERT FREQUENCIES TO SOUND
     # https://glowingpython.blogspot.com/2011/09/sound-synthesis.html
     def frequency_to_sound(self, frequency_data: list,duration = DURATION_PER_PIXEL, sample_rate = SAMPLE_RATE) -> None:
+        print("6. CONVERTING FREQUENCIES TO SOUND...")
         try:
             samples = []
             number_of_audo_samples = int(sample_rate * duration)
@@ -129,8 +110,28 @@ class SoundImageMapper:
         except Exception as e:
             print(f"ERROR GENERATING SOUND: {e}")
 
+class SoundToImage:
+    def __init__( self, sound_path, save_path) -> None :
+        # FIRST CHECK IF SOUND_FILE_PATH EXISTS
+        try:
+            if not os.path.isfile(sound_path):
+                raise FileNotFoundError(f"SOUND FILE NOT FOUND: {sound_path}")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            return
+        
+        # CHECK IF DIRECTORIES EXIST, IF NOT CREATE THEM
+        dir_name = os.path.dirname(save_path)
+        if dir_name and not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+
+        # ASSIGN PATHS
+        self.sound_path = sound_path
+        self.save_path = save_path
+
     def audio_file_to_chunks(self, chunk_sample = int(SAMPLE_RATE * DURATION_PER_PIXEL)) -> tuple :
         try:
+            print("1. LOADING AUDIO FILE AND EXTRACTING CHUNKS...")
             y , sr = librosa.load(self.sound_path, sr=SAMPLE_RATE)
             # print(f"LEN OF SAMPLES IN AUDIO FILE: {len(y)}")
             num_chunks = len(y) // chunk_sample
@@ -146,6 +147,7 @@ class SoundImageMapper:
 
     # EXTRACT FREQUENCIES FROM SOUND
     def sound_to_frequency(self, chunks: list , sr:int) -> list:
+        print("2. EXTRACTING FREQUENCIES FROM AUDIO CHUNKS...")
         try:
             detected_frequencies = []
             for chunk in chunks:
@@ -157,10 +159,21 @@ class SoundImageMapper:
             print(f"ERROR: {e}")
             return []
 
-
+    # MAP FREQUENCIES BACK TO PIXELS
+    def frequency_to_pixel(self, frequency_data: list) -> list:
+        print("3. MAPPING FREQUENCIES BACK TO PIXELS...")
+        f_min, f_max = FREQ_RANGE
+        p_min, p_max = PIXEL_RANGE
+        pixels = [
+            p_min + ((freq - f_min) / (f_max - f_min)) * (p_max - p_min)
+            for freq in frequency_data
+        ]
+        return pixels
+    
     # CONVERT FREQUENCIES BACK TO IMAGE
     # https://stackoverflow.com/questions/71968442/convert-data-pixels-to-image
     def sound_to_image(self, frequency: list, image_size: tuple = (50, 50)) -> None:
+        print("4. CONVERTING FREQUENCIES BACK TO IMAGE...")
         try:
             pixel_values = self.frequency_to_pixel(frequency)
             width, height = image_size
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from converter.image_sound_mapper import SoundImageMapper
+from converter.image_sound_mapper import ImageToSound , SoundToImage
 
-converter = SoundImageMapper(
-    image_path="src_image/image.png",
-    grayscale_path="images/grayscale_image.png",
-    resized_path="images/resized_image.png",
-    sound_path="sounds/frequency.wav",
-    save_path="outputs/frequency_image.png",
-)
+IMAGE_PATH = "src_image/image.png"
+GRAYSCALE_PATH = "images/grayscale_image.png"
+RESIZED_PATH = "images/resized_image.png"
 
-converter.compress_image_with_new_dimension((50, 50))
-converter.image_to_grayscale()
-pixels = converter.pixel_value_extractor()
-freqs = converter.pixel_to_frequency(pixels)
-converter.frequency_to_sound(freqs)
-detected_chunks , detected_sr = converter.audio_file_to_chunks()
-detected_freqs = converter.sound_to_frequency(detected_chunks , detected_sr)
-converter.sound_to_image(detected_freqs)
+SOUND_PATH = "sounds/frequency.wav"
+SAVE_PATH = "reconstructed_image/reconstructed_image.png"
+
+
+its = ImageToSound(IMAGE_PATH,GRAYSCALE_PATH,RESIZED_PATH,SOUND_PATH)
+
+its.compress_image_with_new_dimension((50, 50))
+its.image_to_grayscale()
+pixels = its.pixel_value_extractor()
+freqs = its.pixel_to_frequency(pixels)
+its.frequency_to_sound(freqs)
+
+sti = SoundToImage(SOUND_PATH,SAVE_PATH)
+
+detected_chunks , detected_sr = sti.audio_file_to_chunks()
+detected_freqs = sti.sound_to_frequency(detected_chunks , detected_sr)
+sti.sound_to_image(detected_freqs)
